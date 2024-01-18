57595036b1b450a4d1bb60da4f50f625a5f3438b
Jordan Schwichtenberg <jordanschwichtenberg@gmail.com>
2024-01-17 19:34:49 -0800

    Finish structs, methods, & interfaces

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module awesomeProject
 
 go 1.21.6
---
+++ perimeter.go
@@ -0,0 +1,43 @@
+package awesomeProject
+
+import (
+	"math"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func Perimeter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+func Area(rect Rectangle) float64 {
+	return rect.Width * rect.Height
+}
